67adf127b9a66f4f5b8f56efba4ed036d27d2915
Hieu Vuong Dinh <85358386+hieuvd99@users.noreply.github.com>
2022-07-04 15:36:18 +0700

    Update AddTwoNumbers.java

--- src/com/example/AddTwoNumbers.java
+++ src/com/example/AddTwoNumbers.java
@@ -6,70 +6,92 @@ Explanation: 342 + 465 = 807.*/
 
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AddTwoNumbers {
 	public static void main(String[] args) {
-		ListNode list1 = new ListNode(2, new ListNode(4, new ListNode(3,null)));
-		ListNode list2 = new ListNode(5, new ListNode(6, new ListNode(4,null)));
+		
+		ListNode list1 = new ListNode(3, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, null))))))))));
+		ListNode list2 = new ListNode(7,null);
 		ListNode list3 = addTwoNumbers(list1,list2);
+		System.out.println("\n");
 		while(list3!= null) {
-			System.out.println(list3.val);
+			System.out.print(list3.val);
 			list3 = list3.next;
 		}
 		
 	}
 	
+	
 	// Add each node, balance(carry) = 1 then add them
 	
-//	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//        int carry = 0;
-//        ListNode emptyNode = new ListNode();
-//        ListNode currentNode = new ListNode();
-//        ListNode result = currentNode;
-//        while (emptyNode != l1 || emptyNode != l2) {
-//            int sum = l1.val + l2.val + carry;
-//            carry = sum / 10;
-//            currentNode = (currentNode.next = new ListNode(sum %= 10));
-//            l1 = null != l1.next ? l1.next : emptyNode;
-//            l2 = null != l2.next ? l2.next : emptyNode;
-//        }
-//        if (carry == 1)
-//            currentNode.next = new ListNode(carry);
-//        return result.next;
-//    }
-	
-	//Convert to integer, add 2 numbers, then convert integer to ListNode
-	
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		int llist1 = 0, llist2 = 0;
-		int llist1Rever = 0, llist2Rever = 0;
-		while (l1 != null || l2 != null) {
-			llist1 = (llist1 + l1.val) * 10;
-			llist2 = (llist2 + l2.val) * 10;
-			l1 = l1.next;
-			l2 = l2.next;
-		}
+        int carry = 0;
+        ListNode emptyNode = new ListNode();
+        ListNode currentNode = new ListNode();
+        ListNode result = currentNode;
+        while (emptyNode != l1 || emptyNode != l2) {
+            int sum = l1.val + l2.val + carry;
+            carry = sum / 10;
+            currentNode = (currentNode.next = new ListNode(sum %= 10));
+            l1 = null != l1.next ? l1.next : emptyNode;
+            l2 = null != l2.next ? l2.next : emptyNode;
+        }
+        if (carry == 1)
+            currentNode.next = new ListNode(carry);
+        return result.next;
+    }
+		
+	//Convert to integer, add 2 numbers, then convert integer to ListNode --->> Limited
+	
+//	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//		long llist1Rever = 0, llist2Rever = 0;
+//		
+//		ArrayList<String> str1 = new ArrayList<>();
+//		ArrayList<String> str2 = new ArrayList<>();
+//		
+//		while (l1 != null) {
+//			str1.add(String.valueOf(l1.val));
+//			l1 = l1.next;
+//		}
+//		while (l2 != null) {
+//			str2.add(String.valueOf(l2.val));
+//			l2 = l2.next;
+//		}
+//		Collections.reverse(str1);
+//		Collections.reverse(str2);
+//		
+//		for (int i = 0; i < str1.size(); i++) { 
+//			llist1Rever = (llist1Rever + Long.parseLong(str1.get(i))) * 10;
+//        }
+//		for (int i = 0; i < str2.size(); i++) { 
+//			llist2Rever = (llist2Rever + Long.parseLong(str2.get(i))) * 10;
+//        }
+//		if (llist1Rever != 0) {
+//			llist1Rever = llist1Rever / 10;
+//		}
+//		if (llist2Rever != 0) {
+//			llist2Rever = llist2Rever / 10;
+//		}
+//		
+//		
+//		long sum = llist1Rever + llist2Rever;
+//		ListNode currentNode = new ListNode();
+//		ListNode result = currentNode;
+//		if (sum == 0) {
+//			currentNode = (currentNode.next = new ListNode(0));
+//		} else {
+//			while (sum > 0) {
+//				long tmpLong = sum % 10;
+//				int tmpInt = (int) tmpLong;
+//				sum /= 10;
+//				currentNode = (currentNode.next = new ListNode(tmpInt));
+//			}
+//		}
+//		return result.next;
+//	}
 
-		llist1 = llist1 / 10;
-		llist2 = llist2 / 10;
-
-		while (llist1 != 0 || llist2 != 0) {
-			int digit1 = llist1 % 10;
-			llist1Rever = llist1Rever * 10 + digit1;
-			llist1 /= 10;
-			int digit2 = llist2 % 10;
-			llist2Rever = llist2Rever * 10 + digit2;
-			llist2 /= 10;
-		}
-		int sum = llist1Rever + llist2Rever;
-		ListNode currentNode = new ListNode();
-		ListNode result = currentNode;
-		while (sum > 0) {
-			int tmp = sum % 10;
-			sum /= 10;
-			currentNode = (currentNode.next = new ListNode(tmp));
-		}
-		return result.next;
-	}
 	
 	public static class ListNode {
 		int val;
